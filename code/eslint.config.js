c9a27ceadeecf324bd7ef305ee2c29bebf5e62e4
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-03-19 16:34:01 +0100

    Remove background API (#15)
    * chore: fix lint
    
    * feat: added endpoint to remove background

--- code/eslint.config.js
+++ code/eslint.config.js
@@ -2,10 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
+  {files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["dist/**/*"],},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
