61aba50f92a5bd4d892b0e4fdedd090b72216ece
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-06-13 12:26:17 +0200

    chore: new action to cleanup the storage

---
+++ code/actions/storage-cleanup/config.ts
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { z } from "zod";
+import { ActionConfig } from "./types";
+
+const actionConfigSchema = z.object({
+  dryRun: z.boolean(),
+  storage: z.object({
+    connectionString: z
+        .string({
+          required_error:
+            "Define the Azure Blob Storage connection string on the environment variable AZURE_STORAGE_CONNECTION_STRING",
+        })
+        .trim(),
+    rooms: z.object({
+      containerName: z
+        .string({
+          required_error:
+            "Define the Azure Blob Storage container name for the rooms data on the environment variable AZURE_STORAGE_ROOMS_CONTAINER_NAME",
+        })
+        .trim(),
+    }),
+    images: z.object({
+      containerName: z
+        .string({
+          required_error:
+            "Define the Azure Blob Storage container name for the images data on the environment variable AZURE_STORAGE_IMAGES_CONTAINER_NAME",
+        })
+        .trim(),
+    }),
+  }),
+});
+
+export function getActionConfig(): ActionConfig {
+  const dryRun = process.env.DRY_RUN === "true";
+  const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+  const roomsContainerName = process.env.AZURE_STORAGE_ROOMS_CONTAINER_NAME;
+  const imagesContainerName = process.env.AZURE_STORAGE_IMAGES_CONTAINER_NAME;
+
+  const storage = {
+    connectionString,
+    rooms: {
+      containerName: roomsContainerName,
+    },
+    images: {
+      containerName: imagesContainerName,
+    },
+  };
+
+  const actionConfig = { dryRun, storage };
+
+  return actionConfigSchema.parse(actionConfig);
+}
