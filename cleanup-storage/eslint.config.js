3d14ed13b02b4b6022ed57a20f901b5fdfbeaedf
Jesus Manuel Piñeiro Cid <60609194+jesusmpc@users.noreply.github.com>
2025-06-17 10:09:39 +0200

    Refactor cleanup-storage to container app (#37)
    * chore: initial commit
    
    * feat: cleanup-storage process, deployed on container apps
    
    * chore: just build cleanup storage image
    
    * chore: update storage cleanup whitelist

---
+++ cleanup-storage/eslint.config.js
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["dist/**/*"],},
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
