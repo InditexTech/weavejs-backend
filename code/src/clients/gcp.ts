ca912e65dc6662a73cc1b9a3514904192b4d252e
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-06-19 18:00:01 +0200

    fix: reuse licence

--- code/src/clients/gcp.ts
+++ code/src/clients/gcp.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { JWT } from "google-auth-library";
 import { getServiceConfig } from "../config/config.js";
 
