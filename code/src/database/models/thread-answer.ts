8d26db41bf3f03b286665f8bb9dcb6d1243d3a19
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2025-09-01 15:45:13 +0200

    chore: fix reuse

--- code/src/database/models/thread-answer.ts
+++ code/src/database/models/thread-answer.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { Sequelize, Model, DataTypes } from "sequelize";
 import { WeaveUser } from "@inditextech/weave-types";
 import { ThreadModel } from "./thread.js";
