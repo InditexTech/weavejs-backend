acc178d43953f3ea1edf1b77571c8fc25bd99b7b
Jesus Manuel Piñeiro Cid <jesusmpc@inditex.com>
2026-05-15 13:08:42 +0200

    feat: update to weave.js 4.0.0

--- code/src/templates/nodes/index.ts
+++ code/src/templates/nodes/index.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 2025 INDUSTRIA DE DISEÑO TEXTIL S.A. (INDITEX S.A.)
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { imageNodeToYjsFormat } from "./image.js";
 import { frameNodeToYjsFormat } from "./frame.js";
 import { debugNodeToYjsFormat } from "./debug.js";
